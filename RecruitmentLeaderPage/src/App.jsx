eac2722cdd5b232593a7aedf7838d3d74bbc48bf
saswatjit ptra <saswatp813@gmail.com>
2024-08-13 15:50:29 +0530

    add 8 file

---
+++ RecruitmentLeaderPage/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import RecruimentTeam from "./Components/RecruitmentTeam";
+
+function App() {
+  return (
+    <>
+      <RecruimentTeam />
+    </>
+  );
+}
+
+export default App;
---
+++ RecruitmentLeaderPage/src/Components/RecruitmentTeam.jsx
@@ -0,0 +1,56 @@
+const RecruimentTeam = () => {
+  return (
+    <>
+      <div className="page1">
+        <h1 className="recruiment"> Recruitment</h1>
+        <h1 className="heading">team leader</h1>
+        <img src="public/Meghaphone.png" className="meghaphone" />
+        <img src="public/2boys.png" className="boys" />
+        <div className="About">
+          <p className="About1">
+            The number of online bettors on 81 lottery platform has exceeded
+            250000 per day, in order to make 81 lottery the most popular, fair
+            and just local brand in India. The company is now recruiting team
+            leaders. Team leaders enjoy all platform benefits + invitation
+            rewards + corresponding level monthly salary (up to{" "}
+            <b className="highlt"> 1,000,000 </b>₹)+ offline activity funds (up
+            to <b className="highlt">1,000,000 </b>₹)
+          </p>
+          <p>
+            {" "}
+            <b>
+              Recruitment: Have management , organization and coordination
+              ability. (The platform will provide certain support)
+            </b>
+          </p>
+          <img src="public/DataTable.png" className="datatbl"></img>
+
+          <p className="middle">
+            Team leaders can also enjoy special rewards provided by the
+            platform. They can get 20 rupees by inviting A-level members to
+            register, and cam get rebate bonuses by inviting A-level members to
+            recharge. There is no upper limiton the numberofinvitations, and
+            settlement is based on the number of people and amount
+          </p>
+
+          <div className="About2">
+            <p>
+              {" "}
+              Note: The higher the level of the team leader, the higher the
+              recharge rewards and betting rebates for subordinate members. For
+              details, please check the corresponding activities. <br />
+              If you have any questions about the event, please consult the
+              marketing manager for details.
+              <br /> 81 lottery reserves the right of final interpretation of
+              this event and reserves the right to modify or terminate this
+              event at any time without prior notice, which applies to all
+              activities of the company.
+            </p>
+          </div>
+        </div>
+        <img src="public/footerImg.png" className="footerImg" />
+      </div>
+    </>
+  );
+};
+export default RecruimentTeam;
