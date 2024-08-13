030623951fe8c9589e07d711699e8e9c68c7fbca
saswatjit ptra <saswatp813@gmail.com>
2024-08-13 15:31:42 +0530

    Team Leader Page

---
+++ TeamLeaderPage/src/App.jsx
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+import AboutP from "./components/AboutP";
+
+function App() {
+  return (
+    <>
+      <AboutP />
+    </>
+  );
+}
+
+export default App;
